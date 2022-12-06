adfbbdd0e6546485ce5b6457f6190afd7072ecb8
mehrdadsamadi <samadimehrdad49@gmail.com>
2022-12-06 12:24:01 +0330

    complete user message

--- src/pages/user/user.message.page.vue
+++ src/pages/user/user.message.page.vue
@@ -5,23 +5,42 @@
             <span class="px-3 fw-bold">پیام ها</span>
             <v-divider style="border-color: rgba(0,0,0,1);"></v-divider>
         </div>
+
+        <div class="row">
+            <div class="col-12 mb-3" v-for="message in messages" :key="message._id">
+                <div class="d-flex gap-2">
+                    <v-avatar color="primary">
+                        <v-icon dark>
+                            mdi-account-circle
+                        </v-icon>
+                    </v-avatar>
+                    <v-sheet
+                        rounded="xl"
+                        width="100%"
+                        elevation="2"
+                        class="py-3 px-5 opacity-05"
+                        :class="{'opacity-1' : !message.seen}"
+                    >
+                        <div class="d-flex flex-column">
+                            <p class="text-muted">{{message.createdAt | calender}}</p>
+                            <p v-text="message.message"></p>
+                        </div>
+                    </v-sheet>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import axios from "axios"
+import filters from '@/mixins/filters.js';
 
 export default {
     name: "userMessagePage",
+    mixins: [filters],
     created() {
-        axios.patch(`user/message/seen/${this.$store.state.user._id}`)
-            .then(({data}) => {
-                this.$store.dispatch("loginUser")
-                this.timeOut = setTimeout(() => {
-                    this.$store.dispatch("checkUnreadMessages")
-                }, 500);
-            })
-            .catch(err => this.handle_error(err))
+        this.seen()
     },
     beforeDestroy() {
         clearTimeout(this.timeOut)
@@ -29,11 +48,30 @@ export default {
     data() {
         return {
             timeOut: null,
+            messages: []
+        }
+    },
+    methods: {
+        seen() {
+            axios.patch(`user/message/seen/${this.$store.state.user._id}`)
+                .then(({data}) => {
+                    this.messages = data.data.messages
+                    this.$store.dispatch("loginUser")
+                    this.timeOut = setTimeout(() => {
+                        this.$store.dispatch("checkUnreadMessages")
+                    }, 500);
+                })
+                .catch(err => this.handle_error(err))
         }
     },
 }
 </script>
 
-<style>
-
+<style scoped>
+    .opacity-05 {
+        opacity: .5;
+    }
+    .opacity-1 {
+        opacity: 1 !important;
+    }
 </style>
